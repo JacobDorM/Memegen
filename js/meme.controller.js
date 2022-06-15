b95c526deb781d0b72fbca0b3abafc17eed507bf
JacobDorM <73810417+JacobDorM@users.noreply.github.com>
2022-06-15 19:44:52 +0300

    seconde and last commit - wednesday 2000
    improving css for mobile, editor is missing

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -1,1 +1,10 @@
 'use strict'
+
+function onInit() {
+  // renderTodos()
+}
+
+function toggleMenu(elBtn) {
+  document.body.classList.toggle('menu-open')
+  document.body.classList.contains('menu-open') ? (elBtn.innerText = 'X') : (elBtn.innerText = '☰')
+}
